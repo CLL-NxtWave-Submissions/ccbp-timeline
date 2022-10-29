0abc76f2908f5216682c4d92e534eef97ec51975
Surya T N <suryatej.nim@yahoo.com>
2022-10-29 15:59:31 +0000

    Implemented JSX element tree in TimelineView component to render the intended UI with CCBP program progress details in a vertical alternating timeline

--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -1,1 +1,49 @@
-// Write your code here
+import {Chrono} from 'react-chrono'
+
+import CourseTimelineCard from '../CourseTimelineCard'
+import ProjectTimelineCard from '../ProjectTimelineCard'
+
+import './index.css'
+
+const TimelineView = props => {
+  const {timelineItemsList} = props
+
+  return (
+    <div className="time-line-view-container">
+      <h1 className="time-line-view-header">MY JOURNEY OF</h1>
+      <h1 className="time-line-view-program-name">CCBP 4.0</h1>
+      <div className="time-line-view-chrono-container">
+        <Chrono mode="VERTICAL_ALTERNATING">
+          {timelineItemsList.map(timelineItemsListEntry => {
+            const {categoryId} = timelineItemsListEntry
+            let timelineItemCardComponent = null
+
+            if (categoryId === 'COURSE') {
+              timelineItemCardComponent = (
+                <div>
+                  <CourseTimelineCard
+                    key={timelineItemsListEntry.id}
+                    courseCardData={timelineItemsListEntry}
+                  />
+                </div>
+              )
+            } else if (categoryId === 'PROJECT') {
+              timelineItemCardComponent = (
+                <div>
+                  <ProjectTimelineCard
+                    key={timelineItemsListEntry.id}
+                    projectCardData={timelineItemsListEntry}
+                  />
+                </div>
+              )
+            }
+
+            return timelineItemCardComponent
+          })}
+        </Chrono>
+      </div>
+    </div>
+  )
+}
+
+export default TimelineView
