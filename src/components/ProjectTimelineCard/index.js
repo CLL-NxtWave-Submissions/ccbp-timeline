bc6cec35dc34aee8623d08608b34eb33a361935f
Surya T N <suryatej.nim@yahoo.com>
2022-10-30 10:23:09 +0000

    Implemented the JSX element tree in ProjectTimelineCard to render the intended UI

--- src/components/ProjectTimelineCard/index.js
+++ src/components/ProjectTimelineCard/index.js
@@ -1,1 +1,38 @@
-// Write your code here
+import {AiFillCalendar} from 'react-icons/ai'
+
+import './index.css'
+
+const ProjectTimelineCard = props => {
+  const {projectCardData} = props
+
+  const {
+    projectTitle,
+    description,
+    imageUrl,
+    duration,
+    projectUrl,
+  } = projectCardData
+
+  return (
+    <>
+      <img
+        className="project-time-line-card-img"
+        src={imageUrl}
+        alt={projectTitle}
+      />
+      <div className="project-name-duration-container">
+        <h1 className="project-name">{projectTitle}</h1>
+        <div className="project-duration-container">
+          <AiFillCalendar className="project-duration-calendar-icon" />
+          <p className="project-duration">{duration}</p>
+        </div>
+      </div>
+      <p className="project-description">{description}</p>
+      <a className="project-link" href={projectUrl}>
+        Visit
+      </a>
+    </>
+  )
+}
+
+export default ProjectTimelineCard
