e6bee8e4f61cf32f7795498fa9c2f11ca9ef062e
Surya T N <suryatej.nim@yahoo.com>
2022-10-30 09:40:33 +0000

    Implemented the JSX element tree in CourseTimelineCard component to render the intended course card UI in the timeline

--- src/components/CourseTimelineCard/index.js
+++ src/components/CourseTimelineCard/index.js
@@ -1,1 +1,29 @@
-// Write your code here
+import {AiFillClockCircle} from 'react-icons/ai'
+import './index.css'
+
+const CourseTimelineCard = props => {
+  const {courseCardData} = props
+  const {courseTitle, description, duration, tagsList} = courseCardData
+
+  return (
+    <>
+      <div className="course-time-line-card-header-container">
+        <h1 className="course-title">{courseTitle}</h1>
+        <div className="course-duration-container">
+          <AiFillClockCircle className="course-duration-clock-icon" />
+          <p className="course-duration">{duration}</p>
+        </div>
+        <p className="course-description">{description}</p>
+        <ul className="course-tag-list">
+          {tagsList.map(tagsListItem => (
+            <li key={tagsListItem.id} className="course-tag">
+              {tagsListItem.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default CourseTimelineCard
