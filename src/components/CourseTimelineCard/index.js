d84e13d01e1bef30c5ca2ff0fd4d8e38999d7e4b
Surya T N <suryatej.nim@yahoo.com>
2022-10-30 10:32:58 +0000

    Adjusted the alignment of JSX elements in CourseTimelineCard, ProjectTimelineCard components through CSS. Changed the JSX element tree layout of CourseTimelineCard component.

--- src/components/CourseTimelineCard/index.js
+++ src/components/CourseTimelineCard/index.js
@@ -13,15 +13,15 @@ const CourseTimelineCard = props => {
           <AiFillClockCircle className="course-duration-clock-icon" />
           <p className="course-duration">{duration}</p>
         </div>
-        <p className="course-description">{description}</p>
-        <ul className="course-tag-list">
-          {tagsList.map(tagsListItem => (
-            <li key={tagsListItem.id} className="course-tag">
-              {tagsListItem.name}
-            </li>
-          ))}
-        </ul>
       </div>
+      <p className="course-description">{description}</p>
+      <ul className="course-tag-list">
+        {tagsList.map(tagsListItem => (
+          <li key={tagsListItem.id} className="course-tag">
+            {tagsListItem.name}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
